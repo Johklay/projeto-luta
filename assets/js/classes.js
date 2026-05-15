5224e59b1f38ae29d89cb3b10991247293c5095d
Johklay <johnklayverton@gmail.com>
2026-05-15 17:01:00 -0300

    criei novos conteudos

---
+++ assets/js/classes.js
@@ -0,0 +1,83 @@
+// Knight ou Sorcerer - Guerreiro ou Mago
+// LittleMonster ou BigMonster
+
+class Character {
+
+    _life = 1;
+    maxLife = 1;
+    attack = 0;
+    defense = 0;
+
+    constructor(name) {
+        this.name = name;
+    }
+
+    get life() {
+        return this._life;
+    }
+    set life(newlife) {
+        this._life = newlife < 0 ? 0 : newlife;
+    }
+}
+
+class Knight extends Character {
+    constructor(name) {
+        super(name);
+        this.life = 100;
+        this.attack = 10;
+        this.defense = 8;
+        this.maxLife = this.life;
+    }
+}
+
+class Sorcerer extends Character {
+    constructor(name){
+        super(name);
+        this.life = 80;
+        this.attack = 15;
+        this.defense = 3;
+        this.maxLife = this.life;
+    }
+}
+
+class LittleMonster extends Character {
+    constructor() {
+        super('Little Monster')
+        this.life = 40;
+        this.attack = 4;
+        this.defense = 4;
+        this.maxLife = this.life;
+    }
+}
+
+class BigMonster extends Character {
+    constructor() {
+        super('Big monster')
+        this.life = 120;
+        this.attack = 16;
+        this.defense = 6;
+        this.maxLife = this.life;
+    }
+}
+
+class Stage {
+    constructor(fighter1, fighter2, fighter1EL, fighter2EL) {
+        this.fighter1 = fighter1;
+        this.fighter2 = fighter2;
+        this.fighter1EL = fighter1EL;
+        this.fighter2EL = fighter2EL;
+    }
+
+    start() {
+        this.update();
+        //TODO: Evento do botão de atacar.
+    }
+
+    update() {
+        //fighter1
+        this.fighter1EL.querySelector('.name').innerHTML = this.fighter1.name;
+
+        //fighter2
+        this.fighter2EL.querySelector('.name').innerHTML = this.fighter2.name;
+    }
+}
